5630da9ecb9171f4b472e3fac64cfc03bafb5f1f
Alexander Rodgers <alexedrodgers@gmail.com>
2019-12-03 20:26:06 -0800

    Accuracy check

--- parseClipsOutput.py
+++ parseClipsOutput.py
@@ -1,6 +1,7 @@
 provideFraud = open("has_fraud.txt", "r")
 providerDict = set()
 num_correct = 0
+len_fraud = set()
 total = 0
 for line in provideFraud:
    providerDict.add(line)
@@ -10,7 +11,8 @@ detectedFraud = open("outputfraudfound.txt", "r")
 for line in detectedFraud:
    index = line.index("FRAUD-DETECTED: ")
    offset = len("FRAUD-DETECTED: ")
-   if index < len(line) and index >= 0:
+   if line not in len_fraud and index < len(line) and index >= 0:
+      len_fraud.add(line)
       if line[index + offset:] in providerDict:
          print("PROVIDER FOUND IN HAS_FRAUD: " + line[offset + index: ])
          num_correct += 1
@@ -18,6 +20,8 @@ for line in detectedFraud:
       else:
          print("PROVIDER FOUND NOT IN HAS_FRAUD: " + line[offset + index: ])
          total += 1
+print(len(providerDict))
+print(len(len_fraud))
 print("Accuracy: {0:.2f}%".format((num_correct/total) * 100))
 detectedFraud.close()
 
