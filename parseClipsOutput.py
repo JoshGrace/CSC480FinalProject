4fbb9348e54f21418a2c8ebfb956a49d2b0c244e
JoshGrace <poshamazing@gmail.com>
2019-12-03 20:51:09 -0800

    mostly fixed parseClipsOutput.py

--- parseClipsOutput.py
+++ parseClipsOutput.py
@@ -1,7 +1,7 @@
 provideFraud = open("has_fraud.txt", "r")
 providerDict = set()
 num_correct = 0
-len_fraud = set()
+detectedFraudDict = set()
 total = 0
 for line in provideFraud:
    providerDict.add(line)
@@ -11,17 +11,18 @@ detectedFraud = open("outputfraudfound.txt", "r")
 for line in detectedFraud:
    index = line.index("FRAUD-DETECTED: ")
    offset = len("FRAUD-DETECTED: ")
-   if line not in len_fraud and index < len(line) and index >= 0:
-      len_fraud.add(line)
+   if line not in detectedFraudDict and index < len(line) and index >= 0:
+      detectedFraudDict.add(line)
       if line[index + offset:] in providerDict:
-         print("PROVIDER FOUND IN HAS_FRAUD: " + line[offset + index: ])
+         # print("PROVIDER FOUND IN HAS_FRAUD: " + line[offset + index: ])
          num_correct += 1
          total += 1
       else:
-         print("PROVIDER FOUND NOT IN HAS_FRAUD: " + line[offset + index: ])
+         # print("PROVIDER FOUND NOT IN HAS_FRAUD: " + line[offset + index: ])
          total += 1
-print(len(providerDict))
-print(len(len_fraud))
-print("Accuracy: {0:.2f}%".format((num_correct/total) * 100))
+print("provider Dict len: " + str(len(providerDict)))
+print("detectedFraudDict " + str(len(detectedFraudDict)))
+print("True Positive Percent: {0:.2f}%".format((num_correct/total) * 100))
+print("Correct Sample Percent: {0:.2f}%".format((len(detectedFraudDict)/len(providerDict)) * 100))
 detectedFraud.close()
 
